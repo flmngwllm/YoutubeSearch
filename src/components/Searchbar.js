20a541b1df4ba5e703d5f205b7d2138d3abf11cc
William Fleming <chaosmegaman@Williams-MacBook-Pro.local>
2019-06-17 16:07:47 -0400

    added a form with an input field

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,2 +1,23 @@
 import React from 'react'
 
+
+class SearchBar extends React.Component{
+
+    state = {
+        term : ''
+    }
+
+
+
+    render(){
+        return(
+            <div>
+                <form>
+                    <input type = 'text' value/>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SearchBar
