5fe1a9d267da1cdb8f0b49558ce03b4e2b977008
William Fleming <chaosmegaman@Williams-MacBook-Pro.local>
2019-06-18 16:20:55 -0400

    adding api and parameters

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,1 +1,13 @@
+import axios from 'axios'
+
 const KEY = 'AIzaSyAAuIynz8NVDa_CvV3B8cimjKeMoNSAmGI'
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    // query string parameters we want to add on to the request
+    params: {
+        part: 'snippet',
+        maResults: 5,
+        key: KEY,
+    }
+});
