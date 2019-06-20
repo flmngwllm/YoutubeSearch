533963c1b3edb2e41c5be30b3a09071bbfef2f2a
William Fleming <chaosmegaman@Williams-MacBook-Pro.local>
2019-06-19 20:39:44 -0400

    added title, description and embedded video on page

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import SearchBar from './Searchbar';
 import youtube from '../api/youtube';
 import VideoList from './VideoList'
+import VideoDetail from './VideoDetail'
 
 
 class App extends React.Component{
@@ -23,13 +24,14 @@ this.setState({videos: response.data.items})
 
 //
 onVideoSelect = (video) => {
-    
+    this.setState({selectedVideo: video})
 }
 
     render(){
         return(
             <div className = 'ui container'>
             <SearchBar onFormSubmit={this.onTermSubmit} />
+            <VideoDetail video={this.state.selectedVideo}/>
             <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
             </div>
         )
